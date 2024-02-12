5d858a5bdc4a65c06bc51147f555385cfe5d473a
ibrahim shokry <147347539+shawkary@users.noreply.github.com>
2024-02-12 21:13:46 +0200

    Update main.dart

--- shop-app/main.dart
+++ shop-app/main.dart
@@ -1,30 +1,16 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_project/layout/news_app/main_cubit/main_states.dart';
 import 'package:flutter_project/layout/shop_app/cubit/cubit.dart';
 import 'package:flutter_project/layout/shop_app/shop_layout.dart';
-import 'package:flutter_project/layout/social_app/cubit/social_cubit.dart';
-import 'package:flutter_project/layout/social_app/social_layout.dart';
-import 'package:flutter_project/modules/Beauty_App/BottomNav/requests_screen/extention/third_screen.dart';
 import 'package:flutter_project/modules/Shop_App/on_boarding/on_boarding.dart';
 import 'package:flutter_project/shared/components/constants.dart';
 import 'package:flutter_project/shared/cubit/cubit.dart';
 import 'package:flutter_project/shared/network/local/cache_helper.dart';
 import 'package:flutter_project/shared/network/remote/dio_helper.dart';
 import 'package:flutter_project/shared/styles/themes.dart';
-import 'package:flutter_project/training_screen.dart';
-import 'layout/beauty_app/beauty_layout.dart';
-import 'layout/beauty_app/cubit/cubit.dart';
-import 'layout/news_app/cubit/cubit.dart';
-import 'layout/news_app/main_cubit/main_cubit.dart';
-import 'layout/news_app/news_layout.dart';
 import 'modules/Shop_App/login_screen/shop_login_screen.dart';
 import 'modules/Shop_App/search/cubit/search_cubit.dart';
-import 'modules/Social_App/login_screen/social_login_screen.dart';
-import 'modules/Social_App/register_screen/social_register_screen.dart';
-import 'modules/Todo_App/new_tasks/new_task_screen.dart';
 import 'modules/users/users_screen.dart';
 
 
