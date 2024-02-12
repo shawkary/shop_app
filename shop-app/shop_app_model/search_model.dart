58eb2742eae8392e473bb77099c1a658c9b215dc
ibrahim shokry <147347539+shawkary@users.noreply.github.com>
2024-02-12 21:15:21 +0200

    Add files via upload

---
+++ shop-app/shop_app_model/search_model.dart
@@ -0,0 +1,67 @@
+class SearchModel {
+  bool? status;
+  Data? data;
+
+  SearchModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+}
+
+class Data {
+  int? currentPage;
+  List<Info>? data;
+  String? firstPageUrl;
+  int? from;
+  int? lastPage;
+  String? lastPageUrl;
+  String? path;
+  int? perPage;
+  int? to;
+  int? total;
+
+
+  Data.fromJson(Map<String, dynamic> json) {
+    currentPage = json['current_page'];
+    if (json['data'] != null) {
+      data = <Info>[];
+      json['data'].forEach((v) {
+        data!.add(Info.fromJson(v));
+      });
+    }
+    firstPageUrl = json['first_page_url'];
+    from = json['from'];
+    lastPage = json['last_page'];
+    lastPageUrl = json['last_page_url'];
+    path = json['path'];
+    perPage = json['per_page'];
+    to = json['to'];
+    total = json['total'];
+  }
+}
+
+class Info {
+  int? id;
+  dynamic price;
+  dynamic oldPrice;
+  int? discount;
+  String? image;
+  String? name;
+  String? description;
+  List<String>? images;
+  bool? inFavorites;
+  bool? inCart;
+
+  Info.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    price = json['price'];
+    oldPrice = json['old_price'];
+    discount = json['discount'];
+    image = json['image'];
+    name = json['name'];
+    description = json['description'];
+    images = json['images'].cast<String>();
+    inFavorites = json['in_favorites'];
+    inCart = json['in_cart'];
+  }
+}
