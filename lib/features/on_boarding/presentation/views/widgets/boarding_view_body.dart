7dd2a88c1adfdbb57532fa8f0b0358b4d125fd10
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-06-06 13:01:32 +0300

    add smoothPageIndicator & FloatingActionButton

--- lib/features/on_boarding/presentation/views/widgets/boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/boarding_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'boarding_model.dart';
 
 
@@ -7,25 +8,52 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PageController controller = PageController();
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: PageView.builder(
-        itemBuilder: (context, index){
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: PageView.builder(
+              onPageChanged: (index) {},
+              controller: controller,
+              itemBuilder: (context, index){
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Image(image: AssetImage('assets/images/shopping.png')),
+                    const SizedBox(height: 20),
+                    Align(alignment: Alignment.bottomLeft,
+                      child: Text(lst[index].title,
+                        style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                    ),
+                    Align(alignment: Alignment.bottomLeft,
+                        child: Text(lst[index].onBoarding, style: const TextStyle(fontSize: 20),))
+                  ],
+                );
+              },
+              itemCount: lst.length,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Image(image: AssetImage('assets/images/shopping.png')),
-              const SizedBox(height: 20),
-              Align(alignment: Alignment.bottomLeft,
-                child: Text(lst[index].title,
-                  style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+              SmoothPageIndicator(
+                  controller: controller,
+                  count: lst.length,
+                effect: const ExpandingDotsEffect(
+                  activeDotColor: Colors.blue,
+                ),
               ),
-              Align(alignment: Alignment.bottomLeft,
-                  child: Text(lst[index].onBoarding, style: const TextStyle(fontSize: 20),))
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                  onPressed: (){},
+                  child: const Icon(Icons.arrow_forward_ios, color: Colors.white,),
+              )
             ],
-          );
-        },
-        itemCount: 3,
+          )
+        ],
       ),
     );
   }
