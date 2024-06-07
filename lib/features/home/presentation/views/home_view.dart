354c0a96802ae5e3b7a0615d55cbd6e4cd21942b
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-06-07 21:31:49 +0300

    add bottom_nav_bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ibrahim_project/features/home/presentation/views/widgets/bottom_nav_bar.dart';
 import 'package:ibrahim_project/features/home/presentation/views/widgets/category_screen.dart';
 import 'package:ibrahim_project/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:ibrahim_project/features/home/presentation/views/widgets/favorites_screen.dart';
@@ -8,22 +7,45 @@ import 'package:ibrahim_project/features/home/presentation/views/widgets/setting
 
 
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+
   @override
-  Widget build(BuildContext context) {
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+    int currentIndex = 0;
     List<Widget> screens = [
       const ProductScreen(),
       const CategoryScreen(),
       const FavoriteScreen(),
       const SettingsScreen(),
     ];
-    int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context),
       body: screens[currentIndex],
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index)
+        {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        currentIndex: currentIndex,
+        backgroundColor: Colors.grey.shade100,
+        selectedItemColor: Colors.lightBlue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(label: 'Product', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Category', icon: Icon(Icons.category)),
+          BottomNavigationBarItem(label: 'Favorites', icon: Icon(Icons.favorite)),
+          BottomNavigationBarItem(label: 'Settings', icon: Icon(Icons.settings)),
+        ],),
     );
   }
 }
