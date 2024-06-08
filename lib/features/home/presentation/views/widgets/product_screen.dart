dd0e99663ac40275e285fc8a571530cdc5cb897c
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-06-08 14:01:55 +0300

    add custom_scroll_view

--- lib/features/home/presentation/views/widgets/product_screen.dart
+++ lib/features/home/presentation/views/widgets/product_screen.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:ibrahim_project/core/utiles/styles.dart';
 import 'package:ibrahim_project/features/home/presentation/views/widgets/products_list_view.dart';
-
 import 'carousal_slider.dart';
 import 'category_list_view.dart';
+
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomCarousalSlider(),
-          SizedBox(height: 10),
-          Text('  Categories', style: Styles.textStyle25,),
-          CategoryListView(),
-          SizedBox(height: 10),
-          Text('  Products', style: Styles.textStyle25,),
-          ProductsListView()
-        ]
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomCarousalSlider(),
+                SizedBox(height: 10),
+                Text('  Categories', style: Styles.textStyle25,),
+                CategoryListView(),
+                SizedBox(height: 10),
+                Text('  Products', style: Styles.textStyle25,),
+              ]
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: ProductsListView(),
+        ),
+      ],
     );
   }
 }
--- lib/features/home/presentation/views/widgets/products_list_view.dart
+++ lib/features/home/presentation/views/widgets/products_list_view.dart
@@ -6,36 +6,44 @@ class ProductsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.symmetric(vertical: 5.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image(
-                      width: 150,
-                        height: 150,
-                        fit: BoxFit.fill,
-                        image: NetworkImage('https://i.natgeofe.com/k/63b1a8a7-0081-493e-8b53-81d01261ab5d/red-panda-full-body_4x3.jpg')),
-                  ),
-                  SizedBox(width: 10),
-                  Column(
+    return ListView.builder(
+      shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: const Image(
+                    width: 150,
+                      height: 150,
+                      fit: BoxFit.fill,
+                      image: NetworkImage('https://i.natgeofe.com/k/63b1a8a7-0081-493e-8b53-81d01261ab5d/red-panda-full-body_4x3.jpg')),
+                ),
+                const SizedBox(width: 10),
+                const Expanded(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Name', style: Styles.textStyle25,),
-                      Text('description', style: Styles.textStyle20,),
+                      Text(
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        'Name', style: Styles.textStyle25,),
+                      Text(
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        'description', style: Styles.textStyle20,),
                     ],
-                  )
-                ],
-              ),
-            );
-          },
-        itemCount: 7,
-      ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      itemCount: 7,
     );
   }
 }
