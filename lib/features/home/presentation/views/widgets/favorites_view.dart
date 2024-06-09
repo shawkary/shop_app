77602211a00c46751103bd093e11abba50665ca2
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-06-09 12:11:17 +0300

    add favorite_view

--- lib/features/home/presentation/views/widgets/favorites_view.dart
+++ lib/features/home/presentation/views/widgets/favorites_view.dart
@@ -1,10 +1,45 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ibrahim_project/core/utiles/styles.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return ListView.builder(
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(top: 8.0, left: 7),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Image(
+                  fit: BoxFit.fill,
+                    height: 200,
+                    width: 180,
+                    image: NetworkImage('https://i.natgeofe.com/k/63b1a8a7-0081-493e-8b53-81d01261ab5d/red-panda-full-body_4x3.jpg')),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('name' , style: Styles.textStyle20,),
+                    const SizedBox(height: 130),
+                    Row(
+                      children: [
+                        const Text('price' , style: Styles.textStyle16),
+                        const SizedBox(width: 5),
+                        Text('old price' , style: Styles.textStyle16.copyWith(
+                            color: Colors.grey, decoration: TextDecoration.lineThrough)),
+                        SizedBox(width: 50),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.favorite, size: 20, color: Colors.grey,))
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      itemCount: 10,
+    );
   }
 }
