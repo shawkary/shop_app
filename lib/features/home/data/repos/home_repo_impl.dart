9f5c4e46bd9d2abfa21bbde620060f5a705ce132
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-06-11 14:58:27 +0300

    implement the method in repos

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,14 +1,30 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:ibrahim_project/core/errors/failuers.dart';
+import 'package:ibrahim_project/core/utiles/dio_helper.dart';
 import 'package:ibrahim_project/features/home/data/model/HomeModel.dart';
 import 'package:ibrahim_project/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo
 {
+  HomeRepoImpl(this.dio);
+  DioHelper dio;
+
+
   @override
-  Future<Either<Failure, HomeModel>> fetchHomeData() {
-    // TODO: implement fetchHomeData
-    throw UnimplementedError();
+  Future<Either<Failure, HomeModel>> fetchHomeData()async {
+    try {
+      var data = await dio.getData(endPoint: 'home');
+      HomeModel homeModel;
+      homeModel = HomeModel.fromJson(data);
+      return right(homeModel);
+    } catch (e) {
+      if(e is DioException){
+        return left(ServerFailure.fromDioException(e));
+      }else{
+        return left(ServerFailure(e.toString()));
+      }
+    }
   }
 
 }
