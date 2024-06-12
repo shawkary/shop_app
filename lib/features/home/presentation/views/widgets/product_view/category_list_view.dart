2542cc622f7b94f792069b53510a04f4fe7a7a40
ibrahimshokry <ibrahimshokry333@gmail.com>
2024-06-12 15:01:35 +0300

    fetch data in category-list_view

--- lib/features/home/presentation/views/widgets/product_view/category_list_view.dart
+++ lib/features/home/presentation/views/widgets/product_view/category_list_view.dart
@@ -1,32 +1,43 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ibrahim_project/core/errors/custom_error_widget.dart';
+import 'package:ibrahim_project/features/home/presentation/manager/category_cubit/cubit.dart';
+import 'package:ibrahim_project/features/home/presentation/manager/category_cubit/states.dart';
 
 class CategoryListView extends StatelessWidget {
   const CategoryListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: Container(
-              width: 120,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(7),
-                  image: const DecorationImage(
-                      image: NetworkImage('https://i.natgeofe.com/k/63b1a8a7-0081-493e-8b53-81d01261ab5d/red-panda-full-body_4x3.jpg'),
-                      fit: BoxFit.fill
+    return BlocBuilder<CategoryCubit, CategoryStates>(
+      builder: (BuildContext context, state) {
+        if (state is SuccessCategoryState) {
+          return SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: CachedNetworkImage(
+                    width: 120,
+                      fit: BoxFit.fill,
+                      imageUrl: state.categoryModel.data!.data![index].image!,
+                    placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
                   )
-              ),
+                );
+              },
+              itemCount: state.categoryModel.data!.data!.length,
+              scrollDirection: Axis.horizontal,
             ),
           );
-        },
-        itemCount: 7,
-        scrollDirection: Axis.horizontal,
-      ),
+        }else if(state is ErrorCategoryState){
+          return Center(child: CustomErrorWidget(errMessage: state.errMessage));
+        }else{
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
